8b22df79fff9ba349e933a57e52f8f033e13ae8d
Jose Torres <devjt99@gmail.com>
2025-01-29 17:43:05 -0800

    Initial Commit

---
+++ FetchTakeHomeProject/FetchTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeProjectApp.swift
+//  FetchTakeHomeProject
+//
+//  Created by Jose Torres on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
